8cd28e238d6451687a84ba9a27d80cc329077d30
aadilshabier <aadilshabier1@gmail.com>
2021-02-26 00:28:22 +0530

    Removed unused imports in home/views.py

--- home/views.py
+++ home/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse, Http404
-from django.views import generic
+from django.shortcuts import render, redirect
 from .models import Post
 
 
