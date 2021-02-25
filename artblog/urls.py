468b990e36439611978bd93aa351877fec66583a
aadilshabier <aadilshabier1@gmail.com>
2021-02-25 23:13:36 +0530

    Small changes

--- artblog/urls.py
+++ artblog/urls.py
@@ -21,6 +21,4 @@ urlpatterns = [
     path('home/', include('home.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
